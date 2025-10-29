92bc3154056e4761dff331f1e90389c97f5ae89c
van2jazz <van2jazz@gmail.com>
2025-10-29 18:18:59 +0100

    Updated app.prop

--- src/main/java/com/countryexchange/country_exchange_api/config/WebConfig.java
+++ src/main/java/com/countryexchange/country_exchange_api/config/WebConfig.java
@@ -1,13 +1,23 @@
 package com.countryexchange.country_exchange_api.config;
 
+import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
 
+import java.time.Duration;
+
 @Configuration
 public class WebConfig {
     @Bean
-    public RestTemplate restTemplate() {
-        return new RestTemplate();
+    public RestTemplate restTemplate(RestTemplateBuilder builder) {
+        return builder
+                .requestFactory(() -> {
+                    var factory = new org.springframework.http.client.SimpleClientHttpRequestFactory();
+                    factory.setConnectTimeout(5000);    // 5 seconds (in milliseconds)
+                    factory.setReadTimeout(10000);      // 10 seconds (in milliseconds)
+                    return factory;
+                })
+                .build();
     }
 }
